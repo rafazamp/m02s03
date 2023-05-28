7f269da14ec9c1a5d91be6500fa0e6c2df6e7566
Rafael Zampieron <rafaelprimieri@hotmail.com>
2023-05-28 20:23:06 -0300

    Exercício 03

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((request, response) => {
+  if (request.url == "/produtos") {
+    switch (request.method) {
+      case "GET":
+        break;
+      case "POST":
+        break;
+    }
+  } else {
+    response.writeHead(200, { "Content-Type": "text/plain; charset: utf-8;" });
+    response.end("O endpoint padrão é '/produtos'");
+  }
 });
 
 PORT = 3000;
